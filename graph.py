41e896f41e4a836aa91fc02841bef9ea1968575e
Antoine Eudes <antoine.eudes@gmail.com>
2018-10-21 14:20:28 +0200

    added solution class

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import random
 from matplotlib import pyplot as plt
 import numpy as np
+from math import sqrt
 
 class Vertex:
     def __init__(self, x, y):
@@ -56,14 +57,34 @@ class Solution:
     def __init__(self, list_of_vertex):
         self.vertex = list_of_vertex
 
+    def __getitem__(self, key):
+        id = key
+        return self.vertex[id]
+
     def disturb(self):
         nb_vertex = len(self.vertex)
         id1 = random.randint(nb_vertex)
         id2 = random.randint(nb_vertex)
-        self.vertex[id1], self.vertex[id2] = self.vertex[id2], self.vertex[id1]
+        self[id1], self[id2] = self[id2], self[id1]
 
+
+    def cost(self):
+        s = 0
+        for i in range(len(self.vertex)-1):
+            s += sqrt((self[i+1].x - self[i].x)**2 + (self[i+1].y - self[i].y)**2)
+        s += sqrt((self[-1].x - self[0].x)**2 + (self[-1].y - self[0].y)**2)
+        return s
+
+    def randomize_solution(self, grid):
+        for i in range(grid.nb_vertex):
+            self[i] = grid[i]
 
 
 if __name__ == '__main__':
     g = Graph(100)
     g.display()
+    list_of_vertex = []
+    for i in range(g.nb_vertex):
+        list_of_vertex.append(g[i])
+    sol = Solution(list_of_vertex)
+    print(sol.cost())
