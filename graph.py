6199543e20299337532da68ec682fa76b48c5eaf
Antoine Eudes <antoine.eudes@gmail.com>
2018-10-21 14:01:07 +0200

    fixed issues, added display method to graph

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 import random
+from matplotlib import pyplot as plt
+import numpy as np
 
-def Graph:
+class Graph:
     def __init__(self, nb_vertex, width=1, height=1):
         self._nb_vertex = nb_vertex
         self._width = width
@@ -8,11 +10,14 @@ def Graph:
         self._vertex = dict()
 
         for id in range(nb_vertex):
-            self._vertex[id] = (random.random()*width, random.random()*height))
+            self._vertex[id] = (random.random()*width, random.random()*height)
 
     @property
     def width(self):
         return self._width
+    @property
+    def nb_vertex(self):
+        return self._nb_vertex
     @property
     def height(self):
         return self._height
@@ -22,3 +27,17 @@ def Graph:
             return self._vertex[idx]
         else:
             raise ValueError("vertex does not exist")
+
+    def display(self):
+        X = np.zeros(self.nb_vertex)
+        Y = np.zeros(self.nb_vertex)
+
+        for id in range(self.nb_vertex):
+            X[id] = self[id][0]
+            Y[id] = self[id][1]
+        plt.scatter(X, Y)
+        plt.show()
+
+if __name__ == '__main__':
+    g = Graph(100)
+    g.display()
