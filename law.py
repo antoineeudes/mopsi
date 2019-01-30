6823582a2c0596d55906ff84ae57920bb016e373
Antoine <antoine.eudes@gmail.com>
2019-01-30 15:10:49 +0100

    prepared for rapsberry computation

--- law.py
+++ law.py
@@ -20,44 +20,33 @@ def savePlot(path):
     plt.show()
 
 
-def MeanLaw(path):
-    return
-        # string = line.strip().split('\t')
+def MeanLaw(path="lawmean.txt", nb_iter=100, nb_acquisition=100):
+    fichier = open(path, 'w')
+    for i in range(5, nb_acquisition+5):
+        s = 0.
+        for j in range(nb_iter):
+
+            g = graph.Graph()
+            g.randomize(i)
+            min_solution = Solution(g)
+
+
+            S = SimulatedAnnealing_log(min_solution)
+
+            time0 = time.time()
+
+            min_solution = S.compute(show=False)
+
+            # print("Cout final reel : {}".format(graph.real_cost(min_solution)))
+            # print("Calculs de distances : {}".format(graph.nb_dist))
+            # print("Temps : {}".format(time.time()-time0))
+
+            s += min_solution.cost()
+        s = s/nb_iter
+        line = str(i) + "\t" + str(s) + "\n"
+        fichier.write(line)
+    fichier.close()
 
 if __name__ == "__main__":
 
-    # savePlot("./data/solution/law.txt")
-    for i in range(10, 2000, 2):
-        g = graph.Graph()
-        g.randomize(i)
-        min_solution = Solution(g)
-
-        # S = SimulatedAnnealing_exp(min_solution, 0.1, 0.99999)
-        # S = SimulatedAnnealing_exp(min_solution)
-        S = SimulatedAnnealing_log(min_solution)
-        # S = SimulatedAnnealing_repeated(min_solution, 100, 0.9, 10000)
-
-        # X, Y = [], []
-        # print(graph.nb_dist)
-        # for vertex in min_solution:
-        #     X.append(vertex.x)
-        #     Y.append(vertex.y)
-        # plt.plot(X, Y)
-        # g.display()
-
-        time0 = time.time()
-
-        min_solution = S.compute()
-
-        print("Cout final reel : {}".format(graph.real_cost(min_solution)))
-        print("Calculs de distances : {}".format(graph.nb_dist))
-        print("Temps : {}".format(time.time()-time0))
-
-        # X, Y = [], []
-        # for vertex in min_solution:
-        #     X.append(vertex.x)
-        #     Y.append(vertex.y)
-        # plt.plot(X, Y)
-        # g.display()
-
-        min_solution.write()
+    MeanLaw()
