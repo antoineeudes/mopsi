398ed659ceb19b900b697f9222e9f99c14099291
Antoine <antoine.eudes@gmail.com>
2019-02-06 15:35:02 +0100

    added simulation of law

--- law.py
+++ law.py
@@ -2,21 +2,50 @@ from algo import *
 from config import PATH_LAW_SALESMAN
 from copy import deepcopy
 from matplotlib import pyplot as plt
+from math import sqrt
 
 
 def savePlot(path):
     fichier = open(path, 'r')
     X = []
     Y = []
+    Z = []
     for line in fichier:
         M = deepcopy(line.strip().split('\t'))
         X.append(float(M[0]))
         Y.append(float(M[1]))
+        Z.append(0.83*sqrt(float(M[0])))
 
     fichier.close()
-    plt.plot(X, Y)
+    plt.plot(X, Y, color="blue")
+    plt.plot(X, Z, color="red")
+
+    plt.xlabel('n')
+    plt.ylabel('simulation de E(L_n)')
+    plt.legend(('simulation', 'gamma*sqrt(n)'))
     plt.grid()
     plt.savefig(path+".png")
+    plt.show()
+
+def findGamma(path):
+    fichier = open(path, 'r')
+    X = []
+    Y = []
+    Z = []
+    for line in fichier:
+        M = deepcopy(line.strip().split('\t'))
+        X.append(float(M[0]))
+        Y.append(float(M[1])/sqrt(float(M[0])))
+
+    fichier.close()
+    plt.plot(X, Y, color="blue")
+    print(Y[-1])
+
+    plt.xlabel('n')
+    plt.ylabel('simulation de E(L_n)/sqrt(n)')
+    plt.ylim(0.5, 1.25)
+    plt.grid()
+    plt.savefig(path+"gamma.png")
     plt.show()
 
 
@@ -48,5 +77,6 @@ def MeanLaw(path="./data/solution/lawmean1.txt", nb_iter=40, nb_acquisition=200)
 
 if __name__ == "__main__":
 
-    MeanLaw()
-    # savePlot("./data/solution/lawmean.txt")
+    # MeanLaw()
+    # findGamma("./data/solution/lawmean.txt")
+    savePlot("./data/solution/lawmean.txt")
